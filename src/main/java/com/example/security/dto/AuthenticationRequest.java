efdecb2d2cd1ac16574b666bf92fd686900cc3a8
hamzakhomri <hamza.khomri36@gmail.com>
2023-08-29 19:41:56 +0100

    create token done but i can signign with them

---
+++ src/main/java/com/example/security/dto/AuthenticationRequest.java
@@ -0,0 +1,13 @@
+package com.example.security.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class AuthenticationRequest {
+    private String email;
+    private  String password;
+}
