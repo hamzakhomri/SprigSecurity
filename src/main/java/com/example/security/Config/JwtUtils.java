8b6c53c6e85131be41ba5a469fa6baf8dcb019cf
hamzakhomri <hamza.khomri36@gmail.com>
2023-08-31 22:13:40 +0100

    Like last one specify the code

--- src/main/java/com/example/security/Config/JwtUtils.java
+++ src/main/java/com/example/security/Config/JwtUtils.java
@@ -3,7 +3,6 @@ package com.example.security.Config;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import org.apache.catalina.authenticator.SavedRequest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -13,13 +12,10 @@ import java.util.Map;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
 
-import static javax.crypto.Cipher.SECRET_KEY;
-
 @Component
 public class JwtUtils {
 
     private final String jwtSignignKey = "yourSecretKeyHere"; // Change this to your actual secret key
-
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
     }
@@ -33,11 +29,14 @@ public class JwtUtils {
         return claims.get(claimName) != null;
     }
 
+//                          2
     public <T> T extractClaim(String token, Function<Claims, T> claimsResolver) {
+        System.out.println("extractClaim");
         final Claims claims = extractAllClaims(token);
         return claimsResolver.apply(claims);
     }
 
+    //                1
     private Claims extractAllClaims(String token) {
         return Jwts.parser().setSigningKey(jwtSignignKey).parseClaimsJws(token).getBody();
     }
@@ -46,18 +45,22 @@ public class JwtUtils {
         return extractExpiration(token).before(new Date());
     }
 
-    public String generateToken(UserDetails userDetails) {
+    private String generateToken(UserDetails userDetails) {
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails);
     }
+    public String generateTokenForUser(UserDetails userDetails) {
+        return generateToken(userDetails);
+    }
 
     private String createToken(Map<String, Object> claims, UserDetails userDetails) {
-        return Jwts.builder()
+        return
+                 Jwts.builder() //crée un jeton JWT à l'aide de la classe Jwts.builder()
                 .setClaims(claims)
                 .setSubject(userDetails.getUsername())
                 .claim("authorities", userDetails.getAuthorities())
                 .setIssuedAt(new Date())
-                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.HOURS.toMillis(25)))
+                .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(25)))
                 .signWith(SignatureAlgorithm.HS256, jwtSignignKey)
                 .compact();
     }
