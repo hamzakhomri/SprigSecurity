b39fb20ee222490b728953b4f2a1961ad55fd75d
hamzakhomri <hamza.khomri36@gmail.com>
2023-08-31 22:13:25 +0100

    Like last one specify the code

--- src/main/java/com/example/security/Config/JwtAuthFilter.java
+++ src/main/java/com/example/security/Config/JwtAuthFilter.java
@@ -28,28 +28,43 @@ public class JwtAuthFilter extends OncePerRequestFilter {
             HttpServletResponse response,
             FilterChain filterChain) throws ServletException, IOException {
 
+        // Extract the "Authorization" header from the HTTP request
         final String authHeader = request.getHeader(AUTHORIZATION_HEADER);
         final String userEmail;
         final String jwtToken;
 
+        // Check if the "Authorization" header is missing or doesn't start with "Bearer"
         if (authHeader == null || !authHeader.startsWith("Bearer")) {
+            // Continue processing the request by passing it to the next filter in the chain
             filterChain.doFilter(request, response);
             return;
         }
 
+        // Extract the JWT token by removing "Bearer " from the header value
         jwtToken = authHeader.substring(7);
+
+        // Extract the user's email from the JWT token
         userEmail = jwtUtils.extractUsername(jwtToken); // TODO to be implemented
 
+        // Check if the user's email is not null and there's no existing authentication
         if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails =  userDao.finduserbyemail(userEmail);
+            // Fetch user details from the database based on the extracted email
+            UserDetails userDetails = userDao.finduserbyemail(userEmail);
 
-            if (jwtUtils.isTokenValid(jwtToken,userDetails)) {
+            // Check if the JWT token is valid for the retrieved user details
+            if (jwtUtils.isTokenValid(jwtToken, userDetails)) {
+                // Create an authentication token for the user
                 UsernamePasswordAuthenticationToken authToken =
                         new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
                 authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+                // Set the authentication token in the Spring Security context
                 SecurityContextHolder.getContext().setAuthentication(authToken);
             }
         }
+
+        // Continue processing the request by passing it to the next filter in the chain
         filterChain.doFilter(request, response);
     }
+
 }
