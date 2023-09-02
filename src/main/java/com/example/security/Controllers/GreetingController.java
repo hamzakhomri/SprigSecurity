f1c7ee900a7d531eec4dedac7107737c7451fd9c
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-02 19:44:30 +0100

    Linked with Front but i cant create multiple token in one user

--- src/main/java/com/example/security/Controllers/GreetingController.java
+++ src/main/java/com/example/security/Controllers/GreetingController.java
@@ -1,14 +1,15 @@
 package com.example.security.Controllers;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/v1/greetings")
+@RequestMapping("/api/v1/auth/greetings")
+@CrossOrigin("http://localhost:8081")
 public class GreetingController {
-
     @GetMapping
     public ResponseEntity<String> sayHello(){return ResponseEntity.ok("Hello from API"); }
 
