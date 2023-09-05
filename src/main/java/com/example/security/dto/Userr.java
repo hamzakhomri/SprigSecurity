ce57c5bb36feda4e4af1b84f5fa253b73a96e5ef
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-05 23:56:42 +0100

    generate token from user and Crypt the password

--- src/main/java/com/example/security/dto/Userr.java
+++ src/main/java/com/example/security/dto/Userr.java
@@ -3,10 +3,9 @@ package com.example.security.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
 @Entity
@@ -15,6 +14,12 @@ public class Userr {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String name;
     private String login;
     private String password;
+
+    @OneToMany(mappedBy = "userr", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Rolee> roles = new ArrayList<>();
+
+    // Constructors, getters, setters, etc.
 }
