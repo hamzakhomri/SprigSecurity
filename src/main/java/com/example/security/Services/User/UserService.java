295418d822659765f4b3914a914c12516e37760a
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-05 23:56:55 +0100

    generate token from user and Crypt the password

--- src/main/java/com/example/security/Services/User/UserService.java
+++ src/main/java/com/example/security/Services/User/UserService.java
@@ -5,6 +5,7 @@ import com.example.security.Services.User.IUserService;
 import com.example.security.dto.Userr;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.net.SocketException;
@@ -14,6 +15,8 @@ import java.util.List;
 @Service
 @Slf4j
 public class UserService implements IUserService {
+    BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+
     @Autowired
     UserRepository userRepository;
 
@@ -21,6 +24,7 @@ public class UserService implements IUserService {
     public Userr createUser(Userr userr) throws UnknownHostException, SocketException {
         try {
             log.info("User was created");
+            userr.setPassword(passwordEncoder.encode(userr.getPassword()));
             return userRepository.save(userr);
         }catch (Exception e){
             log.error("error from create User : "+e);
