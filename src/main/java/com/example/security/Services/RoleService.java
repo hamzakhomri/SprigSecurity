c1822b789cc303a691d4150e38f0a5c193d3407e
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-05 15:50:36 +0100

    Creation Role

---
+++ src/main/java/com/example/security/Services/RoleService.java
@@ -0,0 +1,36 @@
+package com.example.security.Services;
+
+import com.example.security.Repository.RoleRepository;
+import com.example.security.dto.Rolee;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.net.SocketException;
+import java.net.UnknownHostException;
+import java.util.List;
+
+@Service
+@Slf4j
+public class RoleService implements IRoleService{
+    @Autowired
+    RoleRepository roleRepository;
+
+    @Override
+    public Rolee createRole(Rolee rolee) throws UnknownHostException, SocketException {
+        try {
+            log.info("Role was created");
+            return roleRepository.save(rolee);
+        }catch (Exception e){
+            log.error("error in creation role : "+e);
+            return null;
+        }
+    }
+
+    @Override
+    public List<Rolee> getallroles() {
+        System.out.println("Get ALlRoles");
+        return roleRepository.findAll();
+    }
+
+}
