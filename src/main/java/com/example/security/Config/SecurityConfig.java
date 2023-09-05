23af4fb6d4417e61051941ccc5bc56d99379ae1d
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-05 23:56:31 +0100

    generate token from user and Crypt the password

--- src/main/java/com/example/security/Config/SecurityConfig.java
+++ src/main/java/com/example/security/Config/SecurityConfig.java
@@ -63,8 +63,8 @@ public class SecurityConfig {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        // return new BCryptPasswordEncoder();
-        return NoOpPasswordEncoder.getInstance();
+        return new BCryptPasswordEncoder();
+        //return NoOpPasswordEncoder.getInstance();
 
     }
 
