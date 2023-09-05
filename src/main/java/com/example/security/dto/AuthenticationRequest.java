6004ee2da596f0599551df49ce71672298962f97
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-05 23:55:58 +0100

    generate token from user and Crypt the password

--- src/main/java/com/example/security/dto/AuthenticationRequest.java
+++ src/main/java/com/example/security/dto/AuthenticationRequest.java
@@ -4,11 +4,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
+
 @Getter
 @Setter
 @NoArgsConstructor
 public class AuthenticationRequest {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String email;
-    private  String password;
+    private String password;
 
+    @ManyToOne
+    @JoinColumn(name = "userr_id")
+    private Userr userr;
+
+    // Constructors, getters, setters, etc.
 }
