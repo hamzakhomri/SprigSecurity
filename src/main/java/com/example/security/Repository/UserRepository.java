1fea4ab6f1c78d8ae403979876ea8e889f4f009b
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-05 23:56:48 +0100

    generate token from user and Crypt the password

--- src/main/java/com/example/security/Repository/UserRepository.java
+++ src/main/java/com/example/security/Repository/UserRepository.java
@@ -5,4 +5,5 @@ import com.example.security.dto.Userr;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<Userr,Long> {
+    Userr findByLogin(String login);
 }
