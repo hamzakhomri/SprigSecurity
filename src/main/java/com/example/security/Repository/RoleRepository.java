3661b2a5f12810db3bf5f5ee0d535eb359e0a787
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-06 14:46:09 +0100

    Cleaning the imported

--- src/main/java/com/example/security/Repository/RoleRepository.java
+++ src/main/java/com/example/security/Repository/RoleRepository.java
@@ -3,7 +3,6 @@ package com.example.security.Repository;
 import com.example.security.dto.Rolee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
 
 public interface RoleRepository extends JpaRepository<Rolee,Long> {
 }
