0b30500fe7a839bf7f2a6a0ba6fd19e45c0af4ce
hamzakhomri <hamza.khomri36@gmail.com>
2023-09-06 18:13:13 +0100

    Refresh Token dont work Missing or invalid 'sub' claim in the token.

--- src/main/java/com/example/security/Config/JwtUtils.java
+++ src/main/java/com/example/security/Config/JwtUtils.java
@@ -19,6 +19,10 @@ import java.util.stream.Collectors;
 @Component
 public class JwtUtils {
     private final String jwtSignignKey = "yourSecretKeyHere"; // Change this to your actual secret key
+    private int refreshExpirationDateInMs;
+    private String secret;
+
+
 
 
     public String extractUsername(String token) {
@@ -61,23 +65,32 @@ public class JwtUtils {
 
 
     private String createToken(Map<String, Object> claims, UserDetails userDetails) {
+        System.out.println("generateToken");
         return
                 Jwts.builder() //crée un jeton JWT à l'aide de la classe Jwts.builder()
                         .setClaims(claims)
                         .setSubject(userDetails.getUsername())
                         .claim("authorities", userDetails.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                         .setIssuedAt(new Date())
-                        .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.MINUTES.toMillis(10)))
+                        .setExpiration(new Date(System.currentTimeMillis() + TimeUnit.SECONDS.toMillis(30)))
                         .signWith(SignatureAlgorithm.HS256, jwtSignignKey)
                         .compact();
     }
     private String generateToken(UserDetails userDetails) {
+        System.out.println("generateToken");
         Map<String, Object> claims = new HashMap<>();
         return createToken(claims, userDetails);
     }
     public String generateTokenForUser(UserDetails userDetails) {
         return generateToken(userDetails);
     }
+    public String doGenerateRefreshToken(Map<String, Object> claims, String subject) {
+        System.out.println("doGenerateRefreshToken");
+        return Jwts.builder().setClaims(claims).setSubject(subject).setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(new Date(System.currentTimeMillis() + refreshExpirationDateInMs))
+                .signWith(SignatureAlgorithm.HS512, secret).compact();
+    }
+
     private Boolean isTokenExpired(String token) {
         try {
             Date expirationDate = extractExpiration(token); // Get the expiration date from the token
@@ -92,27 +105,19 @@ public class JwtUtils {
 
             String timeDifference = String.format("%02dH:%02dM", hours, minutes); // Format hours and minutes as HH:mm
 
-            System.out.println("You Token was Expired after: " + timeDifference); // Print the time difference
+            System.out.println("You Token 4 was Expired after: " + timeDifference); // Print the time difference
 
 
             return expirationDate.before(currentDate);
         } catch (ExpiredJwtException ex) {
-            System.out.println("Token Expired");
+            System.out.println("Token Expired 3");
             return false;
         }
     }
+
     public Boolean isTokenValid(String token, UserDetails userDetails) {
         final String email = extractUsername(token);
         System.out.println("Hello "+email+" welcome to your token");
         return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
     }
-
-
-
-
-
-
-
-
-
 }
